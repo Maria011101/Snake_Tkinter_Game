2ef2342d616f7026e5962685ff895e6e442641e7
t15527mn <maria.niculae@student.manchester.ac.uk>
2020-12-01 12:18:48 +0000

    Created a Rules page

--- Coursework 02/Piton.py
+++ Coursework 02/Piton.py
@@ -19,6 +19,7 @@ def play_game():
     global height
     global portals_placed
     global portals
+    global Obstacles
 
 
     menu_window.destroy()
@@ -26,13 +27,9 @@ def play_game():
     def setWindowDimensions(w,h):
         window = Tk() #create window
         window.title("Snake Game") #title of window
-        # ws = window.winfo_screenwidth() # computers screen size
-        # hs = window.winfo_screenheight()
-        # x = (ws/2) - (w/2) # calculate center
-        # y = (hs/2) - (h/2)
-        # window.geometry('%dx%d+%d+%d' % (w, h, x, y)) # window size
         window.geometry("1080x1920")
         return window
+
 
     def placeFood():
         global food1
@@ -42,12 +39,12 @@ def play_game():
         global food2X
         global food2Y
 
-        food1 = canvas.create_rectangle( 0, 0, snakeSize, snakeSize, fill = "steel blue" )
+        food1 = canvas.create_rectangle( 0, 0, snakeSize, snakeSize, fill = "#FEFFA5" )
         food1X = random.randint(0, width-snakeSize)
         food1Y = random.randint(0, height-snakeSize)
         canvas.move(food1, food1X, food1Y)
 
-        food2 = canvas.create_rectangle( 0, 0, snakeSize, snakeSize, fill = "#824670" )
+        food2 = canvas.create_rectangle( 0, 0, snakeSize, snakeSize, fill = "#931F1D" )
         food2X = random.randint(0, width-snakeSize)
         food2Y = random.randint(0, height-snakeSize)
         canvas.move(food2, food2X, food2Y)
@@ -86,11 +83,11 @@ def play_game():
         elif positions[0][1] < 0:
             canvas.coords(snake[0], positions[0][0],height, positions[0][2], height-snakeSize)
 
-    #Updating the coordonates of the snake's head
+    	#Updating the coordonates of the snake's head
         positions.clear()
         positions.append(canvas.coords(snake[0]))
 
-    #Moving the snake
+    	#Moving the snake
         if direction == "left":
             canvas.move(snake[0], -snakeSize,0)
         elif direction == "right":
@@ -123,7 +120,7 @@ def play_game():
         for i in range(len(snake)-1):
             canvas.coords(snake[i+1], positions[i][0], positions[i][1], positions[i][2], positions[i][3])
 
-    #Looping through the function if gameOver is False
+    	#Looping through the function if gameOver is False
         if 'gameOver' not in locals():
             window.after(90, moveSnake)
         else:
@@ -142,32 +139,33 @@ def play_game():
         canvas.move(food2, food2X, food2Y)
 
     def growSnake(grow_2blocks = False):
-    #we add a block at the end of the snake, depending on the direction
+
+    	#we add a block at the end of the snake, depending on the direction
         lastElement = len(snake)-1
         lastElementPos = canvas.coords(snake[lastElement])
-        snake.append(canvas.create_rectangle(0, 0, snakeSize, snakeSize, fill="#FDF3F3"))
+        snake.append(canvas.create_rectangle(0, 0, snakeSize, snakeSize, fill="#ABC798"))
 
-#adding  a block to the right of the last one if the direction is left
+		#adding  a block to the right of the last one if the direction is left
         if (direction == "left"):
             canvas.coords(snake[lastElement+1], lastElementPos[0] + snakeSize, lastElementPos[1], lastElementPos[2] + snakeSize, lastElementPos[3])
 
-#adding  a block to the left of the last one if the direction is right
+		#adding  a block to the left of the last one if the direction is right
         elif (direction == "right"):
             canvas.coords(snake[lastElement+1], lastElementPos[0] - snakeSize, lastElementPos[1], lastElementPos[2] - snakeSize, lastElementPos[3])
 
-#adding  a block under the last one if the direction is up
+		#adding  a block under the last one if the direction is up
         elif (direction == "up"):
             canvas.coords(snake[lastElement+1], lastElementPos[0], lastElementPos[1] + snakeSize, lastElementPos[2], lastElementPos[3]+snakeSize)
 
-#adding  a block above the last one if the direction is down
+		#adding  a block above the last one if the direction is down
         else:
             canvas.coords(snake[lastElement+1], lastElementPos[0], lastElementPos[1]-snakeSize, lastElementPos[2], lastElementPos[3]-snakeSize)
 
-    #We add the second block only if grow_2blocks is True (the snake has eaten the red food)
+    	#We add the second block only if grow_2blocks is True (the snake has eaten the red food)
         if grow_2blocks == True:
             lastElement = len(snake)-1
             lastElementPos = canvas.coords(snake[lastElement])
-            snake.append(canvas.create_rectangle(0, 0, snakeSize, snakeSize, fill="#FDF3F3"))
+            snake.append(canvas.create_rectangle(0, 0, snakeSize, snakeSize, fill="#ABC798"))
 
             if (direction == "left"):
                 canvas.coords(snake[lastElement+1], lastElementPos[0]+snakeSize, lastElementPos[1], lastElementPos[2]+snakeSize, lastElementPos[3])
@@ -192,30 +190,56 @@ def play_game():
             return True
         else:
             return False
+    	
 
+    # def createObstacle(x, y):
+    # 	global Ob
+    # 	Ob = canvas.create_rectangle(x, y, x + 20, y + 100, fill = "white")
 
+    # 	def moveObstacle():
+    # 		global Ob
+    # 		for i in range(1000):
+    # 			canvas.move(Ob, 10, 10)
 
-     def endScreen():
+    # 	canvas.after(1000, moveObstacle)
+
+    def endScreen():
         global end_screen
         global end_score
 
-        end_screen = canvas.create_rectangle(0, 0 , 800, 800)
-        end_screen.pack()
+        end_screen = canvas.create_rectangle(0, 0 , 1000, 850, fill = "#ABC798")
 
-        end_score = Label(window, text = "Your Score:" + str(score), font = ("Times 20 italic bold"))
+        game_over = Label(window, text = "GAME OVER!", font = ("Times 50 italic bold"), bg = "#ABC798", fg = "#1A1F16")
+        game_over.place( x= 200 , y= 200)
 
+        end_score = Label(window, text = "Your Score: " + str(score), font = ("Times 30 italic bold"), bg = "#ABC798", fg = "#1A1F16")
+        end_score.place( x= 200 , y= 300)
+
+        back_to_menu_button = Button(text = "Back to Menu", bg = "#5D737E", activebackground = "#D68C45", height = 2 ,width = 15, font = ("Arial, 15"), command = backToMenu)
+        back_to_menu_button.place( x = 200, y = 400)
+
+    def CheatCode1(event):
+    	global score
+    	score += 20
+    	txt = "Score:" + str(score)
+    	canvas.itemconfigure(scoreText, text = txt)
+
+    def backToMenu():
+        window.destroy()
+        menu_page()
+    
 
     width = 1000 # width of snake’s world
     height = 850 # height of snake’s world
 
     #creating the window
     window = setWindowDimensions(width, height)
-    canvas = Canvas(window, bg="black", width=width, height=height)
+    canvas = Canvas(window, bg="#1A1F16", width=width, height=height)
 
     #Creating the snake
     snake = []
     snakeSize = 20
-    snake.append(canvas.create_rectangle(snakeSize, snakeSize, snakeSize * 2, snakeSize * 2, fill = "white" ))
+    snake.append(canvas.create_rectangle(snakeSize, snakeSize, snakeSize * 2, snakeSize * 2, fill = "#60992D" ))
 
     score = 0 #the score starts at 0
     txt = "Score:" + str(score)
@@ -226,13 +250,26 @@ def play_game():
     canvas.bind("<Right>", rightKey)
     canvas.bind("<Up>", upKey)
     canvas.bind("<Down>", downKey)
+    canvas.bind("p", CheatCode1)
     canvas.focus_set()
     direction = "right"
+
+    # createObstacle(100,10)
 
 
     placeFood()
     moveSnake()
+
+   
+
     window.mainloop()
+
+#gets you back to the menu from the rules page
+def backfrules():
+	global rule_window
+	rule_window.destroy()
+	menu_page()
+
 
 #This displays the menu page
 def menu_page():
@@ -240,7 +277,10 @@ def menu_page():
     global username_window
 
     #destroying the enter username window
-    username_window.destroy()
+    try:
+        username_window.destroy()
+    except:
+        pass
 
     menu_window = Tk()
     menu_window.title("P I T O N")
@@ -253,17 +293,20 @@ def menu_page():
 
     #Menu Buttons
 
-    playButton = Button(text = "Start", bg = "#5D737E", activebackground = "#D68C45", height = 2 ,width = 15, font = ("Arial, 15"), command = play_game)
+    playButton = Button(menu_window, text = "Start", bg = "#5D737E", activebackground = "#D68C45", height = 2 ,width = 15, font = ("Arial, 15"), command = play_game)
     playButton.place( x = 200, y = 300)
 
-    rulesButton = Button(text = "Rules", bg = "#5D737E", activebackground = "#D68C45", height = 2 ,width = 15, font = ("Arial, 15"))
+    rulesButton = Button(menu_window, text = "Rules", bg = "#5D737E", activebackground = "#D68C45", height = 2 ,width = 15, font = ("Arial, 15"), command = rules_page)
     rulesButton.place( x = 200, y = 400)
 
-    settingsButton = Button(text = "Settings", bg = "#5D737E", activebackground = "#D68C45", height = 2 ,width = 15, font = ("Arial, 15"))
-    settingsButton.place( x = 200, y = 500)
+    leaderboardButton = Button(menu_window, text = "Leaderboard", bg = "#5D737E", activebackground = "#D68C45", height = 2 ,width = 15, font = ("Arial, 15"))
+    leaderboardButton.place( x = 200, y = 500)
 
-    exitButton = Button(text = "Exit", bg = "#5D737E", activebackground = "#D68C45", height = 2 ,width = 15, font = ("Arial, 15"), command = menu_window.destroy)
-    exitButton.place( x = 200, y = 600)
+    settingsButton = Button(menu_window, text = "Settings", bg = "#5D737E", activebackground = "#D68C45", height = 2 ,width = 15, font = ("Arial, 15"))
+    settingsButton.place( x = 200, y = 600)
+
+    exitButton = Button(menu_window, text = "Exit", bg = "#5D737E", activebackground = "#D68C45", height = 2 ,width = 15, font = ("Arial, 15"), command = menu_window.destroy)
+    exitButton.place( x = 200, y = 700)
 
     #Menu picture of snake
     pythonImage = PhotoImage(file = "snake.png")
@@ -273,6 +316,24 @@ def menu_page():
 
 
     menu_window.mainloop()
+
+def rules_page():
+	global rule_window
+	global menu_window
+
+	menu_window.destroy()
+
+	rule_window = Tk()
+	rule_window.title("Rules")
+	rule_window.geometry("1080x1920")
+	rule_window.configure( bg = "#ABC798")
+
+	Label(rule_window, text = "Game Rules", bg = "#ABC798", fg = "#1A1F16", font = ("Arial, 50")).place(x = 300, y = 50)
+
+	backButton = Button(rule_window, text = "Back", bg = "#5D737E", activebackground = "#D68C45", height = 1 ,width = 5, font = ("Arial, 15"), command = backfrules)
+	backButton.place( x = 50, y = 10)
+
+	rule_window.mainloop()
 
 #A page where we get the username
 def enter_username_page():
@@ -286,14 +347,14 @@ def enter_username_page():
         else:
             menu_page()
 
-#Creating the page and configuring it
+	#Creating the page and configuring it
     username_window = Tk()
     username_window.title("Welcome to PITON!")
     username_window.geometry("350x250")
     username_window.configure(bg = "#AEC5EB")
     Label(text = "Enter username:", bg = "#AEC5EB", font = "Arial", fg = "#3A405A", pady =20).pack()
 
-#Creating the entry
+	#Creating the entry
     username = Entry(username_window, width = 30, borderwidth = 2, bg = "#F9DEC9")
     username.pack()
 
@@ -312,3 +373,4 @@ def enter_username_page():
 
 
 enter_username_page()
+
